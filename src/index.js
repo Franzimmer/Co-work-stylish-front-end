dcb4f2fab3e13ba3e2146d7fc3eb773ba91c2c0e
max80713 <max80713@hotmail.com>
2022-05-22 19:22:52 +0800

    setup basic routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        {/* <Route index element={<Home />} />
+        <Route path="product" element={<Product />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="profile" element={<Profile />} /> */}
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
