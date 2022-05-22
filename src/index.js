0226fae8482420854b84e7a3f3bfabcbd7d29322
max80713 <max80713@hotmail.com>
2022-05-23 01:13:32 +0800

    refine Home page component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import App from './App';
+import Home from './pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
-        {/* <Route index element={<Home />} />
-        <Route path="product" element={<Product />} />
+        <Route index element={<Home />} />
+        {/* <Route path="product" element={<Product />} />
         <Route path="cart" element={<Cart />} />
         <Route path="profile" element={<Profile />} /> */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   </BrowserRouter>
